a1a5b905353059d7e7b815fa5840da8cc96cda9a
judge_happily <you3192001@gmail.com>
2024-02-10 01:22:56 +0900

    Initial Commit

---
+++ WeatherApp/TeststerGroup/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Jae hyung Kim on 2/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+    
+}
+
